a99c4b2e795f223613fa62701f0f3296ff3f86ce
SamGreenan <samgreenan1234567@gmail.com>
2021-02-17 15:13:02 +0000

    API connectivity with shoes appearing on page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from functools import wraps
 import pymongo
 import folium
 import requests
+import json
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -68,20 +69,61 @@ def homepage():
 def homescreen():
     return render_template("homescreen.html")
 
-#@app.route("/APIsearch")
-#@login_manditory
-#def shoesearch():
- #   return render_template("shoesearch.html")
+@app.route("/APIsearch")
+@login_manditory
+def shoesearch():
+    return render_template("shoesearch.html")
 
-#@app.route("/apiresult", methods=["POST"])
-#@login_manditory
-#def apiresult():
+   
+
+@app.route("/apiresult", methods=["POST"])
+@login_manditory
+def apiresult():
     #getting the form value
-    #shoes = request.form['shoe']
- #   r = requests.get('')
-  #  json_object = r.text
-   # return json_object
-    #return render_template("apiresult.html")
+    brandname = request.form['brand']
+    gendertype = request.form['gender']
+    r = requests.get('https://api.thesneakerdatabase.com/v1/sneakers?limit=100&brand='+brandname+'&gender='+gendertype)
+    #json_object = json.loads(r['results'])
+    json_object = r.json()
+
+
+    #for key in json_object:
+        #print (key, ":", json_object[key])
+        
+        
+        # json_object['results'][i]['shoe']
+    
+    #sneaker 1 
+    name_result = json_object['results'][0]['name']
+    shoe_result = json_object['results'][0]['media']['imageUrl']
+    price_result = json_object['results'][0]['retailPrice']
+    #sneaker 2
+    name_result2 = json_object['results'][1]['name']
+    shoe_result2 = json_object['results'][1]['media']['imageUrl']
+    price_result2 = json_object['results'][1]['retailPrice']
+
+    #sneaker 3
+    name_result3 = json_object['results'][2]['name']
+    shoe_result3 = json_object['results'][2]['media']['imageUrl']
+    price_result3 = json_object['results'][2]['retailPrice']
+
+    #sneaker 4
+    name_result4 = json_object['results'][3]['name']
+    shoe_result4 = json_object['results'][3]['media']['imageUrl']
+    price_result4 = json_object['results'][3]['retailPrice']
+
+    #sneaker 5
+    name_result5 = json_object['results'][4]['name']
+    shoe_result5 = json_object['results'][4]['media']['imageUrl']
+    price_result5 = json_object['results'][4]['retailPrice']
+
+    #return shoe_result
+    return render_template('shoesearch.html', 
+    imageUrl=shoe_result, name=name_result, price=price_result,
+    imageUrl2=shoe_result2, name2=name_result2,price1=price_result2,
+    imageUrl3=shoe_result3, name3=name_result3,price2=price_result3,
+    imageUrl4=shoe_result4, name4=name_result4,price3=price_result4,
+    imageUrl5=shoe_result5, name5=name_result5,price4=price_result5)
 
 
 @app.route("/maptest")
